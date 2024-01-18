16caa42813d12eefd7058a1259cfa3b216ddd2b4
Nikhil Arya <nikhilakabob@gmail.com>
2024-01-18 20:26:02 +0530

    expense tracker project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </div>
+  );
+}
+
+export default App;
